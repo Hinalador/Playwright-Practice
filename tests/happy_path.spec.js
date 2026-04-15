594fe5f26484f1460c8b1534cd57a9aef9b8c178
Hinalador <jacevedosalazar@gmail.com>
2026-04-14 23:58:19 -0500

    Refactor: mejorar locators y agregar beforeEach en tests

--- tests/happy_path.spec.js
+++ tests/happy_path.spec.js
@@ -4,14 +4,15 @@ test.beforeEach(async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     await page.getByTestId('username').fill('standard_user');
     await page.getByTestId('password').fill('secret_sauce');
-    await page.getByTestId('login-button').click();
+    await page.getByRole('button', { name: 'Login' }).click();
 });
 
 test('Validar que el contador del carrito se actualiza a 1', async ({ page }) => {
-    await page.getByTestId('add-to-cart-sauce-labs-backpack').click();
+    await page.locator('.inventory_item')
+        .filter({ hasText: 'Sauce Labs Backpack' })
+        .getByRole('button', { name: 'Add to cart' })
+        .click();
     await expect(page.getByTestId('shopping-cart-badge')).toHaveText('1');
-
-    // await page.pause(); 
 });
 
 /* const { test, expect } = require('@playwright/test');
